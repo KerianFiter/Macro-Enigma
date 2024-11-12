b92ea9a633b1926fc65307ee5e3ded299bed5bcb
Mathys GAROUI <mathys.garoui@gmail.com>
2024-11-12 16:57:36 -0500

    nage

--- Assets/UnderwaterController.cs
+++ Assets/UnderwaterController.cs
@@ -10,15 +10,46 @@ public class UnderwaterController : MonoBehaviour
     Vector3 initialLeftHandPosition = Vector3.zero;
     Vector3 initialRightHandPosition = Vector3.zero;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed = 5.0f;
+    private Rigidbody rb;
+    public CapsuleCollider headCollider;
+    public Transform headTransform;
+
+    // Start is called before the first frame update
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // NAGE
+        Vector2 thumbstick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+    
+        // Set collider trigger based on thumbstick movement
+        headCollider.isTrigger = thumbstick == Vector2.zero;
+
+        // Calculate movement based on head forward direction
+        float step = Time.deltaTime * speed;
+        Vector3 forward = headTransform.forward * thumbstick.y * step;
+        Vector3 right = headTransform.right * thumbstick.x * step;
+        Vector3 movement = forward + right;
+
+        rb.AddForce(movement, ForceMode.Force);
+
+        // Rotate the player based on button presses
+        if (OVRInput.GetDown(OVRInput.RawButton.X))
+        {
+            transform.rotation *= Quaternion.Euler(0, -30, 0);
+        }
+
+        if (OVRInput.GetDown(OVRInput.RawButton.Y))
+        {
+            transform.rotation *= Quaternion.Euler(0, 30, 0);
+        }
+        
+        // UNDERWATER
         bool hasCollided = false;
         for (int i = 0; i < chainControllers.Count; i++)
         {
